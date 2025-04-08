3e18e9e98e497e4b2ca2859ee2be3b94f7062c75
Damião Roberto <106033055+Damiaoroberto@users.noreply.github.com>
2025-04-08 10:33:15 +0200

    Add files via upload

---
+++ MindfulnessApp/Program.cs
@@ -0,0 +1,37 @@
+﻿// ADD Exceeding Requirement
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Start Visualization Activity");
+            Console.WriteLine("5. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "5") break;
+
+            Activity activity = choice switch
+            {
+                "1" => new BreathingActivity(),
+                "2" => new ReflectionActivity(),
+                "3" => new ListingActivity(),
+                "4" => new VisualizationActivity(),
+                _ => null
+            };
+
+            if (activity != null)
+                activity.Run();
+        }
+    }
+}
